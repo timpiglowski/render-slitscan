3eb4e658387e7c6b15ffa3f808fc14f6e2c6c8dc
Tim <quasare-sekanten.4s@icloud.com>
2022-03-15 20:14:07 +0100

    Improved file treatment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # render-slitscan
+
+### Notes
+1. This app only supports PNG-images as input. Therefore it should be at the very end of your post-processing workflow.
+2. Normally, the file will be built from the bottom. This means, that frames early in the animation will be part of the lower image, while the later frames will result in the top of the output image. To reverse this use the `--invert` flag
--- SLITSCAN.py
+++ SLITSCAN.py
@@ -1,58 +1,40 @@
 import cv2
 import os
 import time
+import glob
 
 user_x_res = int(input("Desired width in px: "))
 user_y_res = int(input("How tall should each slice be? In px: "))
 
-def execution_time(start, end, function):
-    print(function + " took: " + str(end - start) + "s")
+input_dir = "./0_RENDER_FILES/"
+output_dir = "./1_CROPPED_FILES/"
 
-def load_images_from_folder(folder):
-    images = []
-    for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename))
-        if img is not None:
-            images.append(img)
-    return images
+"""
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
 
-def CROP(path_to_img, filename):
-    img = cv2.imread(path_to_img)
-    export_path = ("./1_CROPPED_FILES/")
-    crop_img = img[0:user_y_res, 0:user_x_res] #y:y+d; x:x+d
-    cv2.imwrite(export_path + "/" + filename+"_cropped.png", crop_img)
-
-def MERGE():
-    files = load_images_from_folder("./1_CROPPED_FILES/")
-    files = files[::-1]
-    im_v = cv2.vconcat(files)
-    cv2.imwrite('./output.png', im_v)
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', type=dir_path)
+args = parser.parse_args()
+"""
 
 if __name__ == '__main__':
-    # COUTING FILES
-    start = time.time()
-    i = 0
-    for filename in os.listdir("./0_RENDER_FILES"):
-        i = i + 1
-    end = time.time()
-    print("found " + str(i) + " files in " + str(end - start) + "s")
+    # GETTING FILES
+    files = sorted(glob.glob(f"{input_dir}*.png"), reverse=False)
+    #print(f"found {len(files)} PNG images. Any other files were ignored.")
 
     #CROP
-    start = time.time()
-    for filename in os.listdir("./0_RENDER_FILES"):
-        CROP("./0_RENDER_FILES/"+filename,filename)
-    end = time.time()
-    execution_time(start,end,"CROP")
-
-    #MERGE
-    start = time.time()
-    MERGE()
-    end = time.time()
-    execution_time(start,end,"MERGE")
-
-    #CLEANUP
-    start = time.time()
-    for f in os.listdir("./1_CROPPED_FILES/"):
-        os.remove(os.path.join("./1_CROPPED_FILES/", f))
-    end = time.time()
-    execution_time(start,end,"CLEANUP")
+    start = time.perf_counter()
+    cropped = []
+    for filename in filter(lambda f: f.lower().endswith(('.jpg', '.jpeg', '.png')), os.listdir(input_dir)):
+        path = input_dir+filename
+        img = cv2.imread(path)
+        crop_img = img[0:user_y_res, 0:user_x_res] #y:y+d; x:x+d
+        cropped.append(crop_img)
+        im_v = cv2.vconcat(cropped)
+        cv2.imwrite('./output2.png', im_v)
+    end = time.perf_counter()
+    print(f"Operation took: {round(end - start, 5)}s")
